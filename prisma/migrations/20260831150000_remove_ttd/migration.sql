b291dbc0030b4a49665cc655d057cbd0952f7103
Apriiansh <apriansyahmlp@gmail.com>
2026-08-31 14:09:02 +0700

    vecel

---
+++ prisma/migrations/20260831150000_remove_ttd/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "dialog_kinerja" DROP COLUMN "ttd_atasan_path",
+DROP COLUMN "ttd_pegawai_path";
+
+-- AlterTable
+ALTER TABLE "reviu" DROP COLUMN "ttd_atasan_path",
+DROP COLUMN "ttd_pegawai_path";
